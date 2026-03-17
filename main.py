fe3e2a1c9c77d0765e4377c820faffab98dab34c
incrediblemet <incrediblemet@gmail.com>
2026-03-18 10:42:59 +1100

    D: group Player class

--- main.py
+++ main.py
@@ -9,24 +9,38 @@ def main():
     color = (0, 0, 0)
     clock = pygame.time.Clock()
     dt = 0
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # grouped objects that can be updated
+    updatable = pygame.sprite.Group()
+
+    # grouped objectst that can be drawn
+    drawable = pygame.sprite.Group()
+
+    # Player is the name of the class, not an instance of it
+    # (This must be done before any Player objects are created)
+    Player.containers = (updatable, drawable)
+    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
+    # game loop
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         dt = clock.tick(60) / 1000
-        player.update(dt)
+        updatable.update(dt)
         
         # rendering
         screen.fill(color)
-        player.draw(screen)
+        for instance in drawable:
+            instance.draw(screen)
         pygame.display.flip() 
         
         
